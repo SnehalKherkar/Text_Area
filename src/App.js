c750e35bb0a891f5d837e5be534609c78030caae
SnehalKherkar <snehalkherkar06@gmail.com>
2023-05-14 23:21:49 +0530

    project created

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [text, setText] = useState("");
+  const [count, setCount] = useState(100);
+  const [warning, setWarning] = useState(false);
+  const [submittedText, setSubmittedText] = useState("");
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+    setText(value);
+    setCount(100 - value.length);
+    if (value.length > 90) {
+      setWarning(true);
+    } else {
+      setWarning(false);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSubmittedText(text);
+    setText("");
+    setCount(100);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <textarea className="textarea"
+          value={text}
+          onChange={handleChange}
+          maxLength={100}
+          style={{
+            borderRadius: "5px",
+            padding: "10px",
+            fontSize: "16px",
+            marginBottom: "10px",
+          }}
+        />
+        <div className="counter">
+          Characters remaining: {count}
+          {warning && <span className="warning" style={{ color: "red" }}> Warning: Only 10% remaining!</span>}
+        </div>
+        <button className="submit" disabled={count < 0} style={{ marginTop: "10px" }}>
+          Submit
+        </button>
+      </form>
+      {submittedText && <p>{submittedText}</p>}
+    </div>
+  );
+};
+
+export default App;
